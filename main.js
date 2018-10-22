70a25a46035b75f2305434f4fdc0570b96d8ef93
Yuri Lee <yurilee9696@gmail.com>
2018-10-22 19:35:44 +0900

    read index

--- main.js
+++ main.js
@@ -1,43 +1,45 @@
-var http = require("http");
-var pdftohtml = require('pdftohtmljs'); //pdf를 html로
-var converter = new pdftohtml('ss.pdf', "index.html");
-converter.convert('ipad').then(function() {
-    console.log("Success");
-}).catch(function(err) {
-    console.error("Conversion error: " + err);
-});
-
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
 var mammoth = require("mammoth");
-let textYuri ="";
-mammoth.extractRawText({path: "./hello.docx"})
+var template = require('./lib/template.js');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+let docText ="";
+
+function readDoc(){
+  //word문서 읽기
+  mammoth.extractRawText({path: "./hello.docx"})
     .then(function(result){
         var text = result.value; // The raw text 
         console.log(text);
         var messages = result.messages;
-        textYuri = text;
+        docText = text;
     })
     .done();
-
-http.createServer(function(request, response){
-    /* 
-        HTTP 헤더 전송
-        HTTP Status: 200 : OK
-        Content Type: text/plain
-    */
-    response.writeHead(200, {'Content-Type':'text/plain; charset=utf-8'});
-    
-    /*
-        Response Body 를 "Hello World" 로 설정
-    */
-    response.end(`${textYuri}`);
-}).listen(8081);
-
-
-
-console.log("Server running at http://127.0.0.1:8081");
-
-
-
-
-
-    
+}
+http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    app.use(bodyParser.urlencoded({ extended:false}));
+    app.use(bodyParser.json());
+    //app.use(express.bodyParser());
+    if(pathname === '/'){
+      fs.readFile('index.html', function(err, data) {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
+      });
+    } else if(pathname === '/choose'){
+      readDoc();
+      response.end('read');
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
+    }
+}).listen(3000);
